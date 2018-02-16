d4a4d8c71c86b3aab6915111beefec8b5bc93dc2
Jeremy Hall <jeremy.hall@toyotaconnected.com>
2018-02-16 09:52:22 -0600

    Init commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Use the following container img
+FROM python:3-alpine
+
+# Copy the Contents of the  CWD on the host to the container
+RUN mkdir /hanzo-python
+
+# Set the CWD to /hanzo-python
+WORKDIR /hanzo-python
+
+# Copy shit over to the docker container to be run
+COPY . /hanzo-python
+
+# Run Some Commands!
+RUN pip3 install -r requrements.txt
+
+# Expose port 9022 for kafka and 443 for datadingle
+EXPOSE 443
+EXPOSE 9092
+
+# Run the Following Command Once Launched
+CMD [ "python3", "/hanzo-python/dd-events.py" ]
---
+++ dd-events.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import time
+import subprocess
+import logging
+from datadog import initialize, api
+
+class DDAGGREGATOR(object):
+    """This class is used to pull alerts from DataDog and push them into a Kafka topic.
+    """
+    def __init__(self):
+        self.options = {"api_key" : 'bf13f3e9b8f4a4487af8d023d154eb36', \
+                    'app_key':'be3c94fd4e997d49b7493c2b3e87c11c15cf84ec'}
+        initialize(**self.options)
+
+    @staticmethod
+    def prep_timing():
+        """This function is used to set the time frame that events will be pulled from
+        datadog. Returns an object with 2 KV pairs, start_time:int() and end_time:int()
+        """
+        end_time = time.time()
+        start_time = end_time - (60 * 60 * 24) #86400 = 1day
+        times = {"start_time": start_time, "end_time": end_time}
+        return times
+
+    def collect_events(self, times):
+        """This function collects the datadog events for a time frame specified by the times
+        object. We pull the full list from the API then format the pieces we want, returning
+        a list.
+        """
+        all_events = api.Event.query(start=times["start_time"], end=times["end_time"])
+        print(all_events)
+
+DDA = DDAGGREGATOR()      
+while True:
+    TIMES = DDA.prep_timing()
+    EVENT_ARRAY = DDA.collect_events(TIMES)
+    print(EVENT_ARRAY)
+    time.sleep(5)
